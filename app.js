16fe239f4aed79dac103fece5df0bee401715122
Jared <weissjmw@gmail.com>
2022-09-03 13:27:33 -0400

    ig open first pass

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const open = require("open");
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -13,6 +14,29 @@ const server = http.createServer((req, res) => {
     // set response content
     res.write("<html><body><p>This is the home Page.</p></body></html>");
     res.end();
+  } else if (req.url == "/instagram") {
+    (async () => {
+      // Opens the image in the default image viewer and waits for the opened app to quit.
+      // await open("unicorn.png", { wait: true });
+      // console.log("The image viewer app quit");
+
+      // Opens the URL in the default browser.
+      await open("instagram://user?username=jaredonajourney");
+      // await open("http://www.instagram.com");
+
+      // Opens the URL in a specified browser.
+      // await open("https://sindresorhus.com", { app: "firefox" });
+
+      // Specify app arguments.
+      // await open("https://sindresorhus.com", {
+      //   app: ["google chrome", "--incognito"],
+      // });
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("<html><body><p>Opened IG in new tab.</p></body></html>");
+      res.end();
+    })();
+    // open("http://www.instagram.com");
+    // open("instagram://user?username=jaredonajourney");
   } else if (req.url == "/student") {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write("<html><body><p>This is student Page.</p></body></html>");
