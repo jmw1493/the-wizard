18847b559544492aed6a42ece8bc0fb7255c37da
Jared <weissjmw@gmail.com>
2022-09-02 22:18:01 -0400

    basic server

---
+++ app.js
@@ -0,0 +1,29 @@
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    //check the URL of the current request
+
+    // set response header
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    // set response content
+    res.write("<html><body><p>This is the home Page.</p></body></html>");
+    res.end();
+  } else if (req.url == "/student") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<html><body><p>This is student Page.</p></body></html>");
+    res.end();
+  } else if (req.url == "/admin") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<html><body><p>This is admin Page.</p></body></html>");
+    res.end();
+  } else res.end("Invalid Request!");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
